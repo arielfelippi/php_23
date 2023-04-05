7dc0c64c5246c34cf69cfefea69c3c706b3dcc23
arielfelippi <ariel.felippi@hotmail.com>
2023-04-04 21:52:57 -0300

    logica pedra papel tesoura

--- exercicios.php
+++ exercicios.php
@@ -71,9 +71,87 @@ echo "Os numeros impares de 11 - 29 são: {$impares}!<br>";
 // 1) calcular os 3 primeiros numeros primos
 
 // 2) criar a logica do jogo: pedra, papel , tesoura.
-// pedra
-// papel
-// tesoura
 
-// as condicoes, quem ganha de quem
+// ATORES
+    // pedra
+    // papel
+    // tesoura
+
+// Condicoes do jogo, quem ganha de quem
 // mostrar quem ganhou
+
+// array|vetor [] associativo. 
+
+/*
+    $condicoes    = [1, 2, 3]; // length|size 3.
+    $condicoes    = ["papel", "tesoura", 'pedra']; // length|size 3.
+    $indice ou $i =   0,        1,         2
+
+
+    $condicoes = [
+        "papel" => [
+            "tesoura" => "perde",
+            "pedra" => "ganha",
+            "papel" => "empate",
+        ],
+        "pedra" => [
+            "tesoura" => "ganha",
+            "pedra" => "empate",
+            "papel" => "perde",
+        ],
+        "tesoura" => [
+            "tesoura" => "empate",
+            "pedra" => "perde",
+            "papel" => "ganha",
+        ],
+    ];
+
+    $condicoes["papel"]["tesoura"]; // perde
+    
+    $daniel = "pedra";    // 1
+    $eduardo = "tesoura"; // 2
+
+    $condicoes[$daniel][$eduardo]; // ganha
+
+
+
+
+
+*/
+
+
+
+
+
+// 1) calcular os 3 primeiros numeros primos
+
+
+
+$primos = "";
+
+for ($numero = 1; $numero <= 10; $numero++)
+{
+    $cont = 0;
+
+    for ($numeroVerificar = $numero; $cont <= $numeroVerificar; $cont++)
+    { 
+        $resto = $numero % 2; // resto da divisao (divisao exata)
+
+        if ($resto == 0) {
+            $cont++;
+        }
+    }
+
+    if ($cont <= 2) {
+        $primos .= "{$numero}";
+    }
+    
+}
+
+
+echo "Os 3 primeiros numeros primos (1 - 10) são: {$primos}!<br>";
+
+
+
+
+
--- teste.php
+++ teste.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 // tudo mesma coisa, soma + 1
 $numero = $numero + 1;
 $numero += 1;
@@ -54,4 +55,35 @@ for ($numero =10; $numero <= 30; $numero++)
 
 echo "Os numeros pares de 10 - 30 são: {$pares}!<br>";
 echo "Os numeros impares de 11 - 29 são: {$impares}!<br>";
+
+*/
+
+
+$primos = "";
+
+for ($numero = 2; $numero <= 10; $numero++)
+{
+    $cont = 0;
+    $numeroVerificar = $numero; // 4
+
+    for ($divisor = 2; $divisor <= $numeroVerificar; $divisor++)
+    { 
+        $multiplo = $numeroVerificar % 2; // elimino os numeros pares
+        $resto = $numeroVerificar % $divisor;
+
+        if ($resto == 0 && $multiplo != 0) {
+            $cont++;
+        }
+    }
+
+    if ($cont == 1) {
+        $primos .= "{$numero} ";
+    }
+    
+}
+
+
+echo "Os 3 primeiros numeros primos no intervalo de 1 - 10 são:<br> <b>{$primos}</b> "; // 1, 3, 5, 7
+
+
 
